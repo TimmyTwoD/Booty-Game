0e4c180878b12aaf5b60032496f1ea7f2cdad3cc
TimmyTwoD <david12345l@gmail.com>
2022-11-02 11:53:22 -0700

    Move
    character sprite moves to where your mouse clicked

---
+++ Booty Game/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Vector2 target;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
